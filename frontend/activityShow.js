3696d6a6aa9e2b6e2d4e5a5c09d9f4f44495a2b8
github username <github email address>
2019-11-12 15:25:49 -0700

    Adds filter for activities based on weather.

--- frontend/activityShow.js
+++ frontend/activityShow.js
@@ -1,12 +1,14 @@
 const activityDiv = document.querySelector(".activityCards")
+const params = new URLSearchParams(window.location.search)
+const temp = params.get("temp")
 
-
-fetch("http://[::1]:3000/activities/")
+fetch(`http://localhost:3000/activities/`)
     .then(response => response.json())
     .then(activities => activities.map(activity => {
-        let h2 = document.createElement("h2")
-
-        h2.innerText = activity.name
-
-        activityDiv.appendChild(h2)
+        const h1 = document.createElement("h1")  
+        if (activity.temperature >= +temp-20 && activity.temperature <= +temp+20) {
+            h1.innerText = activity.name
+        }
+    
+        activityDiv.appendChild(h1)
     }))
