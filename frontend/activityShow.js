8c3308fbef3de407e7ecabf2e8de42c1d3fbbce2
github username <github email address>
2019-11-11 22:13:52 -0700

    Fixes showActivity.html. Adds weather picture selection logic. Adds Degree and F signs to the temperature.

--- frontend/activityShow.js
+++ frontend/activityShow.js
@@ -1,10 +1,12 @@
 const activityDiv = document.querySelector(".activityCards")
 
 
-fetch("http://[::1]:3000/activities")
+fetch("http://[::1]:3000/activities/")
     .then(response => response.json())
     .then(activities => activities.map(activity => {
         let h2 = document.createElement("h2")
+
         h2.innerText = activity.name
+
         activityDiv.appendChild(h2)
     }))
