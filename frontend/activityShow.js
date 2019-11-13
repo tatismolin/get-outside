2dd2456a14725accce226cdec78768f3405a61e5
slzander <staceyzander@gmail.com>
2019-11-12 17:36:55 -0700

    crud methods

--- frontend/activityShow.js
+++ frontend/activityShow.js
@@ -5,10 +5,26 @@ const temp = params.get("temp")
 fetch(`http://localhost:3000/activities/`)
     .then(response => response.json())
     .then(activities => activities.map(activity => {
-        const h1 = document.createElement("h1")  
+        const h1 = document.createElement("h1")
+        const img = document.createElement("img")
+        const edit = document.createElement('a')
+        const deleteButton = document.createElement('button')
+
         if (activity.temperature >= +temp-20 && activity.temperature <= +temp+20) {
             h1.innerText = activity.name
-        }
-    
-        activityDiv.appendChild(h1)
+
+            img.setAttribute("src", activity.photo)
+            edit.innerText = "EDIT"
+            edit.href = `activityEdit.html?id=${activity.id}`
+
+            deleteButton.innerText = "DELETE"
+            deleteButton.addEventListener("click", function(event){
+                event.target.parentNode.remove()
+                fetch(`http://localhost:3000/activities/${activity.id}`, {
+                    method: "DELETE"
+                })
+            })
+
+            activityDiv.append(h1, img, edit, deleteButton)
+        }  
     }))
