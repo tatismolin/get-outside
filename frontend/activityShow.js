d6dc320d3db1fec25e9b191e3f8f12b630a17fe4
slzander <staceyzander@gmail.com>
2019-11-13 13:50:51 -0700

    fixes show activity cards styling in css

--- frontend/activityShow.js
+++ frontend/activityShow.js
@@ -6,13 +6,15 @@ const temp = params.get("temp")
 fetch(`http://localhost:3000/activities/`)
     .then(response => response.json())
     .then(activities => activities.map(activity => {
-        const h1 = document.createElement("h1")
-        const img = document.createElement("img")
-        const editButton = document.createElement('button')
-        const deleteButton = document.createElement('button')
-        const activityDiv = document.createElement("div")
+
 
         if (activity.temperature >= +temp-20 && activity.temperature <= +temp+20) {
+            const h1 = document.createElement("h1")
+            const img = document.createElement("img")
+            const addButton = document.createElement('button')
+            const editButton = document.createElement('button')
+            const deleteButton = document.createElement('button')
+            const activityDiv = document.createElement("div")
             h1.innerText = activity.name
             img.setAttribute("src", activity.photo)
 
@@ -31,6 +33,6 @@ fetch(`http://localhost:3000/activities/`)
             })
             activityDiv.setAttribute("class", "activity")
             activityDiv.append(img, h1, editButton, deleteButton)
+            activitiesDiv.appendChild(activityDiv)
         }  
-        activitiesDiv.appendChild(activityDiv)
     }))
